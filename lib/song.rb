085949a2379c259c1baa0568e4c6d85701878f57
Dave Winniford <dwinniford@gmail.com>
2020-01-29 15:09:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
     @@all 
   end 
   
-  def Song.new_by_file_name(file_name)
+  def self.new_by_file_name(file_name)
     file_name_array = file_name.split(" - ")
     song_name = file_name_array[1]
     song_ins = Song.new(song_name)
