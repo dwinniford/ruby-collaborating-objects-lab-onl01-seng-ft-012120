c1a27afa24ee57dc380223cc12be51fd7abe9dca
Dave Winniford <dwinniford@gmail.com>
2020-01-29 15:06:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,10 +15,10 @@ class Song
   def Song.new_by_file_name(file_name)
     file_name_array = file_name.split(" - ")
     song_name = file_name_array[1]
-    song = Song.new(song_name)
+    song_ins = Song.new(song_name)
     artist_name = file_name_array[0]
     artist_ins = Artist.find_or_create_by_name(artist_name)
-    
+    artist_ins.add_song(song_ins)
     
   end 
   
